6621fbbfb3f579ecb09f9741cac2bcf3cfe4ed84
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-29 21:20:28 -0800

    modify styles for About

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import { about, mySkills } from "../data";
+import image from "../img/about.jpg";
 
 const About = props => {
   const lang = props.language;
@@ -9,14 +10,21 @@ const About = props => {
     <React.Fragment>
       <div className="about wrapper page">
         <h2>About me</h2>
-        <div className="">{about.main[lang]}</div>
-        <div className="">
-          <h3>Skills</h3>
-          <ul>
-            {mySkills.map(skill => (
-              <li key={skill}>{skill}</li>
-            ))}
-          </ul>
+        <div className="about-container">
+          <div className="image">
+            <img src={image} alt="" />
+          </div>
+          <div className="main-content">
+            <div className="">{about.main[lang]}</div>
+            <div className="">
+              <h3>Skills</h3>
+              <ul className="skills">
+                {mySkills.map(skill => (
+                  <li key={skill}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
