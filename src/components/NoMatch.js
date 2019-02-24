cd42706d80b665a53860cbe3750a1eb30ef8eb07
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-02-24 00:17:53 -0800

    modify style for a:focus on header

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -12,7 +12,7 @@ const NoMatch = props => {
             ? 'Sorry, but what you are looking for could not be found.'
             : 'お探しのページが見つかりませんでした。'}
         </p>
-        <Link to='/' class='button ui secondary'>
+        <Link to='/' className='button ui secondary'>
           Home
         </Link>
       </div>
