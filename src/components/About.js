d9610dfef72da5bb04b707debf2ff676a186bbea
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-13 22:09:36 -0700

    replace old content with new one on About component

--- src/components/About.js
+++ src/components/About.js
@@ -10,20 +10,35 @@ const About = props => {
         <h2>About me</h2>
         <div className='about-container'>
           <div className='main-content'>
-            <div>{about.main[lang]}</div>
-            <div>
+            <div className='description'>{about.main[lang]}</div>
+            <div className='skills'>
               <h3>{about.skills[lang]}</h3>
-              <ul className='skills'>
-                {mySkills.map(skill => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
-              <h3>{about.interests[lang]}</h3>
-              <ul className='skills'>
-                {interests[lang].map(skill => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
+              <div className='skills-container'>
+                <div>
+                  <h4>Frontend</h4>
+                  <ul>
+                    {about.sortedSkills['Frontend'].map(skill => (
+                      <li key={skill}>{skill}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <h4>Backend</h4>
+                  <ul>
+                    {about.sortedSkills['Backend'].map(skill => (
+                      <li key={skill}>{skill}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <h4>Others</h4>
+                  <ul>
+                    {about.sortedSkills['others'].map(skill => (
+                      <li key={skill}>{skill}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
